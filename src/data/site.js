69c83f2e91ecae0c03d4546a99042cf0176e5d46
Austin Smithson <9phqjvsppv@privaterelay.appleid.com>
2026-09-06 23:08:19 -0500

    Bye Week label, W/L colors, record display on schedule card

--- src/data/site.js
+++ src/data/site.js
@@ -7,7 +7,7 @@ export const schedule = [
   { gameDate: '2026-10-10', date: 'OCT 10', day: 'SAT', time: 'Night',        home: true,  opponent: 'Georgia',         city: 'Tuscaloosa, Ala.',  result: '' },
   { gameDate: '2026-10-17', date: 'OCT 17', day: 'SAT', time: 'Flex',         home: false, opponent: 'Tennessee',       city: 'Knoxville, Tenn.',  result: '' },
   { gameDate: '2026-10-24', date: 'OCT 24', day: 'SAT', time: 'Flex',         home: true,  opponent: 'Texas A&M',       city: 'Tuscaloosa, Ala.',  result: '' },
-  { gameDate: '2026-10-31', date: 'OCT 31', day: 'SAT', time: '',             home: null,  opponent: 'Open Date',       city: '',                  result: '' },
+  { gameDate: '2026-10-31', date: 'OCT 31', day: 'SAT', time: '',             home: null,  opponent: 'Bye Week',       city: '',                  result: '' },
   { gameDate: '2026-11-07', date: 'NOV 7',  day: 'SAT', time: 'Flex',         home: false, opponent: 'LSU',             city: 'Baton Rouge, La.',  result: '' },
   { gameDate: '2026-11-14', date: 'NOV 14', day: 'SAT', time: 'Early',        home: false, opponent: 'Vanderbilt',      city: 'Nashville, Tenn.',  result: '' },
   { gameDate: '2026-11-21', date: 'NOV 21', day: 'SAT', time: '1 p.m. CT',   home: true,  opponent: 'Chattanooga',     city: 'Tuscaloosa, Ala.',  result: '' },
